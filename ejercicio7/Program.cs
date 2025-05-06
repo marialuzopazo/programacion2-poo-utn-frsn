5552d28a6f506bcf001b7a9aadfe5bbd3f9fecdf
programacion.lulaculinquieto <91832154+marialuzopazo@users.noreply.github.com>
2025-05-06 18:56:10 -0300

    Add files via upload

---
+++ ejercicio7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerPOO7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //lista o colecciones
+            Console.WriteLine("**** Lista de objetos de diferentes clases");
+            List<figura> lista = new List<figura>();
+            lista.Add(new Rectangulo(color.rojo, 10, 5, ubicacion.derecha));
+            lista.Add(new Rectangulo(color.blanco, 7, 15, ubicacion.centro));
+            lista.Add(new Rectangulo(color.azul, 3, 2, ubicacion.izquierda));
+            lista.Add(new Rectangulo(color.azul, 8, 4, ubicacion.derecha));
+            lista.Add(new Cuadrado(color.rojo, 10, ubicacion.derecha));
+            lista.Add(new Cuadrado(color.blanco, 7, ubicacion.centro));
+
+            foreach (figura fig in lista)
+            {
+                Console.WriteLine($"Perimetro: {fig.perimetro} Superf: {fig.superficie}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicio7/figura.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EjerPOO7
+{
+    public enum color
+    {
+        blanco,
+        negro,
+        azul,
+        rojo
+    }
+
+    public enum ubicacion
+    {
+        arriba,
+        abajo,
+        derecha,
+        izquierda,
+        centro
+    }
+    internal abstract class figura
+    {
+        private color _colorRelleno;
+        private ubicacion _posicion;
+
+        public abstract int perimetro{
+            get;
+        }
+
+        public abstract int superficie
+        {
+            get;
+        }
+
+        public color colorRelleno
+        {
+            get { return this._colorRelleno; }
+            set { this._colorRelleno = value; }
+        }
+
+        public ubicacion posicion
+        {
+            get { return this._posicion; }
+            set { this._posicion = value; }
+        }
+
+        public virtual void MostrarDatos()
+        {
+            Console.WriteLine($"Color: {this.colorRelleno}");
+            Console.WriteLine($"Posicion: {this.posicion.ToString()}");
+        }
+
+
+    }
+}
