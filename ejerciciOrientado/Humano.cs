87024e1cbbae0220ec31acae3996ca2c39fe8eb4
programacion.lulaculinquieto <91832154+marialuzopazo@users.noreply.github.com>
2025-05-06 16:06:25 -0300

    Add files via upload

---
+++ ejerciciOrientado/Humano.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerPOO1Bis
+{
+    internal class Humano
+    {
+        private string _apellido;
+        private int _edad;
+        private string _direccion;
+
+        public Humano()
+        {
+            this.apellido = "Garcia";
+            this.edad = 20;
+            this.direccion = string.Empty;
+        }
+
+        public Humano (string ape, int ed, string dir)
+        {
+            this.apellido = ape;
+            this.edad = ed;
+            this.direccion = dir;
+        }
+
+        public Humano(string ape, int ed)
+        {
+            this.apellido = ape;
+            this.edad = ed;
+            this.direccion = string.Empty;
+        }
+
+        public string apellido
+        {
+            get { return this._apellido; }
+            set { this._apellido = value; }
+        }
+
+        public int edad
+        {
+            get { return this._edad; }
+            set {
+                if (value >= 0 && value < 150)
+                {
+                    this._edad = value;
+                }
+            }
+        }
+
+        public string direccion
+        {
+            get { return this._direccion;}
+            set { this._direccion = value; }
+        }
+    }
+}
---
+++ ejerciciOrientado/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerPOO1Bis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Humano h1; //declarando
+            h1 = new Humano(); //instanciando
+
+            Humano h2 = new Humano("perez",50,"av. savio 99");   
+           
+            h1.apellido = "gomez";
+            h1.edad = 50000;
+            h1.direccion = "bolivar 222";
+            
+            Console.WriteLine($"Su apellido es: {h1.apellido}");
+            Console.WriteLine($"Su edad es: {h1.edad}");
+            Console.WriteLine($"Su direccion es: {h1.direccion}");
+
+            Console.WriteLine($"Su apellido es: {h2.apellido}");
+            Console.WriteLine($"Su edad es: {h2.edad}");
+            Console.WriteLine($"Su direccion es: {h2.direccion}");
+
+            Console.ReadKey();
+        }
+    }
+}
