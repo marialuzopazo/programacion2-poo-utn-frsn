a46a1a99b43980b19cc8b7246319940cf2745af8
programacion.lulaculinquieto <91832154+marialuzopazo@users.noreply.github.com>
2025-05-08 13:03:59 -0300

    Update Program.cs

--- ejercicio7/Program.cs
+++ ejercicio7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +10,14 @@ namespace EjerPOO7
     {
         static void Main(string[] args)
         {
-            //lista o colecciones
+            // Mostramos un título en consola
             Console.WriteLine("**** Lista de objetos de diferentes clases");
+
+            // Creamos una lista de objetos de tipo figura (la clase base)
             List<figura> lista = new List<figura>();
+
+            // Agregamos distintos objetos a la lista (rectángulos y cuadrados)
+            // Usamos polimorfismo: aunque la lista es de "figura", puede almacenar "Rectangulo" y "Cuadrado"
             lista.Add(new Rectangulo(color.rojo, 10, 5, ubicacion.derecha));
             lista.Add(new Rectangulo(color.blanco, 7, 15, ubicacion.centro));
             lista.Add(new Rectangulo(color.azul, 3, 2, ubicacion.izquierda));
@@ -20,11 +25,13 @@ namespace EjerPOO7
             lista.Add(new Cuadrado(color.rojo, 10, ubicacion.derecha));
             lista.Add(new Cuadrado(color.blanco, 7, ubicacion.centro));
 
+            // Recorremos la lista y mostramos el perímetro y superficie de cada figura
             foreach (figura fig in lista)
             {
                 Console.WriteLine($"Perimetro: {fig.perimetro} Superf: {fig.superficie}");
             }
 
+            // Pausa para que no se cierre la consola
             Console.ReadKey();
         }
     }
