45d84b5cc78a1400f60c3402703107456872814f
programacion.lulaculinquieto <91832154+marialuzopazo@users.noreply.github.com>
2025-05-12 22:30:04 -0300

    Add files via upload

---
+++ ejercicio8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerPOO8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BatidoraDeMano bati = new BatidoraDeMano();
+            bati.CantCuchillas = 3;
+
+            Console.WriteLine($"Cuchillas: {bati.CantCuchillas}");
+            //bati.Batir();
+
+            MultiProcesadora multi = new MultiProcesadora();
+            
+            Console.ReadKey();
+        }
+    }
+}
